816c704fb8e08eff1b955b83a27964f8e20cf3bc
Gamze Özdemir <gamzeozdemiir1@gmail.com>
2025-04-17 23:43:05 +0300

    [Feat]

---
+++ untitled0.py
@@ -0,0 +1,33 @@
+ifade = "gelecegi_yaziyoruz"
+
+
+ifade[0:2]
+
+ifade = "Merhaba!"
+ifade = ifade.lower()
+ifade = ifade.replace("!","")
+ifade
+"10" + 2
+a = "bu uzun bir metindir"
+a[2:5]
+print("uzaya", "git", sep = "**")
+
+a = "bu uzun bir metindir"
+a[8]
+"_Python_".strip("_")
+type(3.14)
+
+a = 5
+b = 10
+c = a*b
+c
+
+ifade = "gelecegi yaziyoruz"
+ifade[1]
+
+ifade = "Merhaba! "
+ifade.strip("")
+"a" + "b" 
+
+degisken = 4
+degisken*degisken
